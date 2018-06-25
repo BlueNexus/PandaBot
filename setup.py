5f9e4b9978009fede19365fb72744e56d0e23e71
BlueNexus <benreeve99@yahoo.co.uk>
2018-06-25 16:56:08 +0100

    fuck

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='PandaBot',
 	author_email='benreeve99@yahoo.co.uk',
 	packages=['PandaBot'],
 	install_requires=['discord.py'],
-	dependency_links = ['https://github.com/Rapptz/discord.py', 'wheel', 'mechanize']
+	dependency_links = ['https://github.com/Rapptz/discord.py', 'wheel', 'mechanicalsoup']
 )
